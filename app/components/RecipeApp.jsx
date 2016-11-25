8f9c5a6915350416213e25ced174a633af4bbc62
piotrq18 <piotrq18@gmail.com>
2016-11-25 14:48:23 +0100

    Add RecipeApp component

---
+++ app/components/RecipeApp.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var RecipeApp = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <p>Recipe App</p>
+      </div>
+    )
+  }
+});
+
+module.exports = RecipeApp;
